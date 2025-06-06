8bc642cc3fb6b3a3e7a995d97deb8caf76f8c686
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-06-06 23:31:57 +0530

    Update supabase_handler.py

--- utils/supabase_handler.py
+++ utils/supabase_handler.py
@@ -1,13 +1,8 @@
-import os
 import pandas as pd
 from supabase import create_client
 
-SUPABASE_URL = "https://futedzabfmyozxcsgqmc.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1dGVkemFiZm15b3p4Y3NncW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTk2OTIsImV4cCI6MjA2NDc5NTY5Mn0.XzEA4TYyeIwfTteg1R5dhobjp0bIR_61-io-59Qc8OM"
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-def fetch_sales_data(df: pd.DataFrame) -> pd.DataFrame:
+def fetch_sales_data(df: pd.DataFrame, url: str, key: str) -> pd.DataFrame:
+    supabase = create_client(url, key)
     results = []
 
     for _, row in df.iterrows():
